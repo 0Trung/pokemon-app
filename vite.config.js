61895605dbb6809afc4166c360dd8a0b481ca5d4
0Trung <nguyenmanhtrung956@gmail.com>
2025-12-12 08:16:30 +0700

    Chuan bi cho Nerlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Thêm dòng 'base' này để Vite biết ứng dụng đang được host
+  // trong thư mục con /pokemon-app/ trên GitHub Pages
   plugins: [react()],
 })
